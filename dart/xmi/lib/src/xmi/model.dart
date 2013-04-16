1f7f6d7b22dbefc486ce3e3033a42ddea6d076b5
Daniel Davidson <dbdavidson@yahoo.com>
2013-04-16 12:17:10 -0500

    track item map in the builder add more of the template meta data

--- dart/xmi/lib/src/xmi/model.dart
+++ dart/xmi/lib/src/xmi/model.dart
@@ -13,8 +13,9 @@ class JsonUModelBuilder {
   /// Data in Map format from parsed json
   Map _modelData;
   
-  /// The current UML entry being built
-  dynamic _current;
+  Map<String, dynamic> _itemMap = {};
+  /// Map of items indexed by xmi:id
+  Map<String, dynamic> get itemMap => _itemMap;
   
 // custom <class JsonUModelBuilder>
 
@@ -45,66 +46,112 @@ class JsonUModelBuilder {
     if(map.containsKey("ownedComment")) {
       Map comment = map["ownedComment"];
       target.comment = new UComment(comment["xmi:id"])..body = comment["body"];
+      _itemMap[target.id] = target;
     }
   }
 
   addProperties(dynamic target, Map map) {
     filterOwnedAttributesBy(map, "uml:Property").forEach((attribute) {
-      print("Found property ${attribute['name']}");
       UProperty property = new UProperty(attribute['xmi:id'])..name = attribute['name'];
       addComment(property, attribute);
+      property.type = attribute['type'];
       property.visibility = attribute['visibility'];
       property.aggregation = attribute['aggregation'];
       target.properties.add(property);
+      _itemMap[property.id] = property;
     });
+  }
+
+  addTemplBindings(dynamic target, Map map) {
+    if(map.containsKey("templateBinding")) {
+      Map bindingEntry = map['templateBinding'];
+      UTemplBinding binding = new UTemplBinding(bindingEntry['xmi:id'])
+        ..signatureId = bindingEntry['signature'];
+      target.templBinding = binding;
+      List substs = asList(bindingEntry["parameterSubstitution"]).where((e) => 
+            xmiType(e) == "uml:TemplateParameterSubstitution").toList();
+      substs.forEach((subst) {
+        UTemplParmSubst ts = new UTemplParmSubst(subst['xmi:id'])
+          ..formalId = subst['formal']
+          ..actualId = subst['actual'];
+        binding.templParmSubsts.add(ts);
+        _itemMap[ts.id] = ts;
+      });      
+      _itemMap[binding.id] = binding;
+    }
+  }
+
+  addTemplSig(dynamic target, Map map) {
+    if(map.containsKey("ownedTemplateSignature")) {
+      Map sigEntry = map['ownedTemplateSignature'];
+      UTemplSig sig = new UTemplSig(sigEntry['xmi:id']);
+      target.templSig = sig;
+
+      if(sigEntry.containsKey("ownedParameter")) {
+        List parms = asList(sigEntry["ownedParameter"]).where((e) => 
+            xmiType(e) == "uml:ClassifierTemplateParameter").toList();
+        parms.forEach((parmEntry) {
+          UClassifierTemplParm tp = new UClassifierTemplParm(parmEntry['xmi:id'])
+            ..name = parmEntry['ownedParameteredElement']['name']
+            ..type = parmEntry['ownedParameteredElement']['xmi:type']
+            ..allowSubstitutable = parmEntry['allowSubstitutable'] == 'true';
+          sig.parms.add(tp);
+          _itemMap[tp.id] = tp;
+        });
+      }
+
+      _itemMap[sig.id] = sig;
+    }
   }
 
   addPackages(dynamic target, Map map) {
     filterPackagedElementsBy(map, "uml:Package").forEach((element) {
-      print("Found package ${element['name']} ${xmiType(element)} => ${element.keys.toList()}");
-      UPackage pkg = new UPackage(element['xmi:id'])..name = element['name'];
+      String pkgName = element['name'];
+      UPackage pkg = new UPackage(element['xmi:id'])
+        ..parentPackageId = target.id
+        ..path = (new List.from(target.path)..add(pkgName))
+        ..name = pkgName;
       addComment(pkg, element);
       target.packages.add(pkg);
       addPackages(pkg, element);
+      _itemMap[pkg.id] = pkg;
     });
     filterPackagedElementsBy(map, "uml:Class").forEach((element) {
-      print("Found class ${element['name']} ${xmiType(element)} => ${element.keys.toList()}");
       UClass klass = new UClass(element['xmi:id'])..name = element['name'];
       addComment(klass, element);
       addProperties(klass, element);
+      addTemplBindings(klass, element);
+      addTemplSig(klass, element);
       target.classes.add(klass);
+      _itemMap[klass.id] = klass;
     });
     filterPackagedElementsBy(map, "uml:Enumeration").forEach((element) {
-      print("Found enum ${element['name']} ${xmiType(element)} => ${element.keys.toList()}");
       UEnumeration enum_ = new UEnumeration(element['xmi:id'])..name = element['name'];
       addComment(enum_, element);
       addProperties(enum_, element);
       target.enums.add(enum_);
+      _itemMap[enum_.id] = enum_;
     });
   }
 
-  UPackage buildPackage(Map map) {
+  UPackage buildRootPackage(Map map) {
     UPackage pkg = new UPackage(map["xmi:id"])..name = map["name"];
     addComment(pkg, map);
     addPackages(pkg, map);
+    _itemMap[pkg.id] = pkg;
     return pkg;
   }
 
   UModel buildModel() {
-
-    final Map<String, dynamic> _handlers = {
-      "uml:Class" : buildPackage,
-      "uml:Package" : buildPackage
-    };
-
     String contents = new File(srcJsonFile).readAsStringSync();
     Map contentMap = JSON.parse(contents);
     _modelData = contentMap["XMI"]["Model"];
 
     UModel model = new UModel();
-    model.root = buildPackage(_modelData);
-
+    model.root = buildRootPackage(_modelData);
+    model._itemMap = itemMap;
     print(prettyJsonMap(model.toJson()));
+    print(model._itemMap);
     return model;
   }
 
@@ -115,7 +162,7 @@ class JsonUModelBuilder {
     return { 
     "srcJsonFile": EBISU_UTILS.toJson(srcJsonFile),
     "modelData": EBISU_UTILS.toJson(_modelData),
-    "current": EBISU_UTILS.toJson(_current),
+    "itemMap": EBISU_UTILS.toJson(_itemMap),
     };
   }
 }
@@ -135,7 +182,6 @@ class UModel {
   Map toJson() { 
     return { 
     "root": EBISU_UTILS.toJson(root),
-    "itemMap": EBISU_UTILS.toJson(_itemMap),
     };
   }
 }
@@ -159,6 +205,10 @@ class UClass {
   /// Properties of this uml class
   List<UProperty> properties = [];
   
+  UTemplBinding templBinding;
+  
+  UTemplSig templSig;
+  
 // custom <class UClass>
 // end <class UClass>
 
@@ -169,17 +219,8 @@ class UClass {
     "name": EBISU_UTILS.toJson(name),
     "comment": EBISU_UTILS.toJson(comment),
     "properties": EBISU_UTILS.toJson(properties),
-    };
-  }
-}
-
-class UClassifierTemplParm { 
-// custom <class UClassifierTemplParm>
-// end <class UClassifierTemplParm>
-
-
-  Map toJson() { 
-    return { 
+    "templBinding": EBISU_UTILS.toJson(templBinding),
+    "templSig": EBISU_UTILS.toJson(templSig),
     };
   }
 }
@@ -294,6 +335,11 @@ class UPackage {
   
   List<UPackage> packages = [];
   
+  String parentPackageId;
+  
+  /// Path to package as list of strings
+  List<String> path = [];
+  
 // custom <class UPackage>
 // end <class UPackage>
 
@@ -306,6 +352,8 @@ class UPackage {
     "classes": EBISU_UTILS.toJson(classes),
     "enums": EBISU_UTILS.toJson(enums),
     "packages": EBISU_UTILS.toJson(packages),
+    "parentPackageId": EBISU_UTILS.toJson(parentPackageId),
+    "path": EBISU_UTILS.toJson(path),
     };
   }
 }
@@ -351,6 +399,9 @@ class UProperty {
   /// Comment (ownedComment) of this uml property
   UComment comment;
   
+  /// Id for the type of this property
+  String type;
+  
   String visibility;
   
   String aggregation;
@@ -364,6 +415,7 @@ class UProperty {
     "id": EBISU_UTILS.toJson(_id),
     "name": EBISU_UTILS.toJson(name),
     "comment": EBISU_UTILS.toJson(comment),
+    "type": EBISU_UTILS.toJson(type),
     "visibility": EBISU_UTILS.toJson(visibility),
     "aggregation": EBISU_UTILS.toJson(aggregation),
     };
@@ -425,34 +477,113 @@ class UStereotype {
 }
 
 class UTemplBinding { 
+  UTemplBinding(
+    this._id
+  ) {
+  }
+  
+  final String _id;
+  /// Id for this uml template binding
+  String get id => _id;
+  
+  /// Id of the signature of this template binding
+  String signatureId;
+  
+  /// Parameter substitutions for this binding
+  List<UTemplParmSubst> templParmSubsts = [];
+  
 // custom <class UTemplBinding>
 // end <class UTemplBinding>
 
 
   Map toJson() { 
     return { 
+    "id": EBISU_UTILS.toJson(_id),
+    "signatureId": EBISU_UTILS.toJson(signatureId),
+    "templParmSubsts": EBISU_UTILS.toJson(templParmSubsts),
     };
   }
 }
 
 class UTemplParmSubst { 
+  UTemplParmSubst(
+    this._id
+  ) {
+  }
+  
+  final String _id;
+  /// Id for this uml template parameter substitution
+  String get id => _id;
+  
+  /// Id of the formal type being substituted
+  String formalId;
+  
+  /// Id of the actual type being substituted
+  String actualId;
+  
 // custom <class UTemplParmSubst>
 // end <class UTemplParmSubst>
 
 
   Map toJson() { 
     return { 
+    "id": EBISU_UTILS.toJson(_id),
+    "formalId": EBISU_UTILS.toJson(formalId),
+    "actualId": EBISU_UTILS.toJson(actualId),
+    };
+  }
+}
+
+class UClassifierTemplParm { 
+  UClassifierTemplParm(
+    this._id
+  ) {
+  }
+  
+  final String _id;
+  /// Id for this uml classifier template parameter
+  String get id => _id;
+  
+  bool allowSubstitutable = false;
+  
+  String name;
+  
+  String type;
+  
+// custom <class UClassifierTemplParm>
+// end <class UClassifierTemplParm>
+
+
+  Map toJson() { 
+    return { 
+    "id": EBISU_UTILS.toJson(_id),
+    "allowSubstitutable": EBISU_UTILS.toJson(allowSubstitutable),
+    "name": EBISU_UTILS.toJson(name),
+    "type": EBISU_UTILS.toJson(type),
     };
   }
 }
 
 class UTemplSig { 
+  UTemplSig(
+    this._id
+  ) {
+  }
+  
+  final String _id;
+  /// Id for this uml template signature
+  String get id => _id;
+  
+  List<UClassifierTemplParm> parms = [];
+  
 // custom <class UTemplSig>
 // end <class UTemplSig>
 
 
   Map toJson() { 
     return { 
+    "id": EBISU_UTILS.toJson(_id),
+    "parms": EBISU_UTILS.toJson(parms),
     };
   }
 }
